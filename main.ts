9e78495b26ee64e5c8146ecb7e347dbc025cfc14
Yasin Mohamed-Fadel Mohamed <132546450+ymohmoh2503@users.noreply.github.com>
2023-06-15 08:38:18 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+basic.pause(2000)
+maqueen.motorStop(maqueen.Motors.M1)
+basic.pause(1000)
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+basic.pause(2000)
+maqueen.motorStop(maqueen.Motors.M2)
+basic.pause(1000)
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+basic.pause(2000)
+maqueen.motorStop(maqueen.Motors.All)
